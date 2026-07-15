687dec9e10d715f8466761ec1bdd677078dfb950
Narenrit Hadsadintorn <kie@adyim.com>
2026-07-15 18:44:36 +0700

    ai-usage-bar 0.3.5

--- Casks/ai-usage-bar.rb
+++ Casks/ai-usage-bar.rb
@@ -1,6 +1,6 @@
 cask "ai-usage-bar" do
-  version "0.3.4"
-  sha256 "329dc6cfb1152ee6d3cb786a64c9ea4d2284ae06ed43a222351723464a3265a8"
+  version "0.3.5"
+  sha256 "1bd081684b55d249bdc4d06193af2b9f6cb86b12e82d059375f165f08716ca3b"
 
   url "https://github.com/captainkie/ai-usage-bar/releases/download/v#{version}/AIUsageBar.zip"
   name "AI Usage Bar"
